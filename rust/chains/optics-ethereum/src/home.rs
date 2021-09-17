96991d3ceaf0817f8ff1433a02e349a13dde22ce
ember arlynx <ember.arlynx@clabs.co>
2021-09-17 01:46:48 -0400

    optics-ethereum has many fewer lines of code

--- rust/chains/optics-ethereum/src/home.rs
+++ rust/chains/optics-ethereum/src/home.rs
@@ -3,14 +3,9 @@
 use async_trait::async_trait;
 use color_eyre::Result;
 use ethers::contract::abigen;
-use ethers::core::types::{Address, Signature, H256};
+use ethers::core::types::{Signature, H256};
 use optics_core::db::DB;
-use optics_core::{
-    traits::{
-        ChainCommunicationError, Common, DoubleUpdate, Home, RawCommittedMessage, State, TxOutcome,
-    },
-    Message, SignedUpdate, Update,
-};
+use optics_core::*;
 use tokio::task::JoinHandle;
 use tokio::time::sleep;
 use tokio::try_join;
@@ -164,10 +159,18 @@ where
 {
     /// Create a reference to a Home at a specific Ethereum address on some
     /// chain
-    pub fn new(name: &str, domain: u32, address: Address, provider: Arc<M>, db: DB) -> Self {
+    pub fn new(
+        provider: Arc<M>,
+        ContractLocator {
+            name,
+            domain,
+            address,
+        }: &ContractLocator,
+        db: DB,
+    ) -> Self {
         Self {
             contract: Arc::new(EthereumHomeInternal::new(address, provider.clone())),
-            domain,
+            domain: *domain,
             name: name.to_owned(),
             db,
             provider,
